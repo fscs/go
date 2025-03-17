ead9c598a869539d20acf49bced88eb9f4e1fe56
0x5a4 <1444@wienstroer.net>
2025-03-17 14:08:53 +0100

    support outputing a pdf

--- flake.nix
+++ flake.nix
@@ -1,67 +1,49 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
       in
-      with pkgs;
       {
-        defaultPackage =
-          # Notice the reference to nixpkgs here.
-          stdenv.mkDerivation rec {
-            buildInputs = [
-              mdbook
-              mdbook-katex
-              mdbook-emojicodes
-              mdbook-d2
-              mdbook-pdf
-              mdbook-plantuml
-              chromium # To create the pdf
-              d2
-              rust-bin.stable.latest.default
-            ];
-            name = "hello";
-            src = self;
-            buildPhase = ''
-              mkdir book
-              mdbook build -d book
-            '';
-            installPhase = "mdbook build -d $out/book";
-            
-          };
-        
-        devShells.default = mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            exa
-            fd
+        defaultPackage = pkgs.stdenv.mkDerivation {
+          name = "fscs-go";
+          src = self;
+
+          buildInputs = with pkgs; [
             mdbook
-            mdbook-katex
             mdbook-emojicodes
-            mdbook-d2
-            mdbook-pdf
-            mdbook-plantuml
-            chromium # To create the pdf
-            d2
-            rust-bin.stable.latest.default
+            mdbook-katex
           ];
 
-          shellHook = ''
-            alias ls=exa
-            alias find=fd
+          buildPhase = ''
+            ${pkgs.lib.getExe pkgs.gnused} -i 's/\[output\.pdf\]//g ' book.toml
+
+            mdbook build
           '';
+
+          installPhase = "mdbook build -d $out/book";
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            chromium # To create the pdf
+            mdbook
+            mdbook-emojicodes
+            mdbook-katex
+            mdbook-pdf
+          ];
         };
       }
     );
